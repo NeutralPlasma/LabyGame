695a98ba8e6c78c8c9462c29e3015e7a0a8cccf2
Gasper <gaspercino.lukman@gmail.com>
2021-10-21 15:05:28 +0200

    Movement
    Redid movement.

--- LabyGame/LabyGame.cpp
+++ LabyGame/LabyGame.cpp
@@ -1,17 +1,14 @@
-﻿// LabyGame.cpp
-
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <conio.h>
 #include <vector>
-
-
 #include <stdlib.h>
 #include <time.h>
-#include <vector>
 #include <fstream>
 #include <thread>
-
+#include <chrono>
+#include <iterator> 
+#include <ctime>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -19,14 +16,17 @@
 #include <unistd.h>
 #endif
 
-#include <iterator>     // std::distance
-
 using namespace std;
 
 #define KEY_UP 72
 #define KEY_DOWN 80
 #define KEY_LEFT 75
 #define KEY_RIGHT 77
+#define W 'w'
+#define S 's'
+#define A 'a'
+#define D 'd'
+
 #define END 27
 #define SPACE 32
 
@@ -35,365 +35,258 @@ using namespace std;
 #define SOUTH 2
 #define WEST 3
 
+#define WALL -37
+#define WALL_DAMAGED 1
+#define WALL_BROKEN 2
 
-#define WALL '#'
 
-//const int WIDTH = 13;
-//const int HEIGHT = 43;
+#pragma region Definicije
 
-const int WIDTH = 61;
-const int HEIGHT = 237;
-string settings[30];
+// menus
+void mainMenu();
+void modeSelector();
+void statsMenu();
 
-ofstream temp("output.txt", ios::trunc);
-ofstream dada("output.txt", ios::app);
+// settings
+void loadSettings();
+void saveSettings();
 
-fstream settings_file("settings.txt", ios::app | ios::in | ios::out);
+// game
+void playerInput();
+void run();
+void generateMap(int x, int y); // rekruzivna funkcija za generacijo mape
+void generatePowerUps();
+void generatePickups();
+
+bool moveUp(int player);
+bool moveDown(int player);
+bool moveLeft(int player);
+bool moveRight(int player);
+
+
+
+// collission logic
+bool isInBounds(int x, int y);
+bool isObstacle(int x, int y);
+bool isPowerUp(int x, int y);
+bool isPoint(int x, int y);
+
+
+// effects
+void explosion(int player);
+void rocket(int player, int direction);
+
+// Graphics
+void render();
+
+
+#pragma endregion Definicije
+
+
+#pragma region Globalne Spremenljivke
+const int WIDTH = 13;
+const int HEIGHT = 43;
+/// FULL SCREEN
+//const int WIDTH = 61;
+//const int HEIGHT = 237;
 
 
 const int buffer_size = WIDTH * HEIGHT * 8;
-bool instaPrint = false;
-
 
 COORD coord;
 
+char map[HEIGHT][WIDTH] = {};
+int playerPositions[2][2] = { {0,0}, {0,0} };
+int players[2] = {'X', 'Y'};
+int powerUps[20] = {};
+int pickups[20] = {};
+
+bool running = false;
 
 
-void clear() {
-    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
-    cout << "\x1B[2J\x1B[H";
+#pragma endregion Globalne Spremenljivke
+
+#pragma region Nastavitve
+ofstream scores("scores.txt", ios::app);
+fstream settingsFile("settings.txt", ios::in);
+
+bool instaPrint = false; // prikaz generiranja labirinta
+
+#pragma endregion Nastavitve
+
+
+
+
+
+
+
+
+bool IsInBounds(int x, int y) {
+    if (x < 0 || x >= WIDTH) return false;
+    if (y < 0 || y >= HEIGHT) return false;
+    return true;
+
+}
+
+
+void print() {
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+    static char buffer[buffer_size];
+    char* p_next_write = &buffer[0];
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < WIDTH; x++) {
+            *p_next_write++ = map[x][y];
+        }
+        *p_next_write++ = '\n';
+    }
+    *p_next_write = '\0';
+    cout.write(&buffer[0], buffer_size);
 }
 
 
 
 
+bool moveUp(int player) {
 
-
-
-
-class Game {
-public:
-    int currentPos[2] = { 1, 1 };
-    char maze[WIDTH][HEIGHT];
-    bool running = false;
-
-
-
-    void generateMaze() {
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                maze[x][y] = WALL;
-            }
-        }
-        currentPos[0] = 1;
-        currentPos[1] = 1;
-
-        this->Visit(1, 1);
-
-        clear();
-
-        maze[1][1] = 'O';
-        maze[WIDTH-2][HEIGHT-1] = ' ';
-        if (instaPrint) {
-            print();
-        }
-
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                dada << maze[x][y];
-            }
-            dada << endl;
-        }
-
-    }
-
-    void Visit(int x, int y)
-    {
-        // Starting at the given index, recursively visits every direction in a
-        // randomized order.
-        // Set my current location to be an empty passage.
-        maze[x][y] = ' ';
-        // Create an local array containing the 4 directions and shuffle their order.
-        int dirs[4];
-        dirs[0] = NORTH;
-        dirs[1] = EAST;
-        dirs[2] = SOUTH;
-        dirs[3] = WEST;
-
-        for (int i = 0; i < 4; ++i)
-        {
-            int r = rand() & 3;
-            int temp = dirs[r];
-            dirs[r] = dirs[i];
-            dirs[i] = temp;
-        }
-
-        // Loop through every direction and attempt to Visit that direction.
-        for (int i = 0; i < 4; ++i)
-        {
-            // dx,dy are offsets from current location. Set them based
-            // on the next direction I wish to try.
-            int dx = 0, dy = 0;
-            switch (dirs[i])
-            {
-                case NORTH: dy = -1; break;
-                case SOUTH: dy = 1; break;
-                case EAST: dx = 1; break;
-                case WEST: dx = -1; break;
-            }
-            // Find the (x,y) coordinates of the grid cell 2 spots
-            // away in the given direction.
-            int x2 = x + (dx << 1);
-            int y2 = y + (dy << 1);
-            if (IsInBounds(x2, y2))
-            {
-                if (maze[x2][y2] == WALL)
-                {
-                    // (x2,y2) has not been visited yet... knock down the
-                    // wall between my current position and that position
-                    maze[x2 - dx][ y2 - dy] = ' ';
-                    // Recursively Visit (x2,y2)
-
-                    //clear();
-                    if (!instaPrint) {
-                        print();
-                    }
-
-                    this->Visit(x2, y2);
-
-
-                }
-            }
-
-            
-        }
-    }
-
-
-
-    void print() {
-        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-        static char buffer[buffer_size];
-        char* p_next_write = &buffer[0];
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                *p_next_write++ = maze[x][y];
-            }
-            *p_next_write++ = '\n';
-        }
-        *p_next_write = '\0';
-        cout.write(&buffer[0], buffer_size);
-    }
-
-
-    void run() {
-        running = true;
-        while (running) {
-            bool work = false;
-            switch (_getch()) {
-                case KEY_UP:
-                    if (moveUp())
-                        work = true;
-                    //generateMaze();
-                    break;
-                case KEY_DOWN:
-                    if (moveDown())
-                        work = true;
-                    //generateMaze();
-                    break;
-                case KEY_LEFT:
-                    if (moveLeft())
-                        work = true;
-                    //generateMaze();
-                    break;
-                case KEY_RIGHT:
-                    if (moveRight())
-                        work = true;
-                    //generateMaze();
-                    break;
-                case END:
-                    running = false;
-                    break;
-                default:
-                    break;
-            }
-            if (work) {
-                //clear();
-                print();
-                if (checkWin(currentPos[0], currentPos[1])) {
-                    clear();
-                    running = false;
-                    cout << "You won!" << endl;
-                }
-            }
-        }
-    }
-
-    bool moveUp() {
-        if (!checkObstacle(currentPos[0]-1, currentPos[1]) &&
-            IsInBounds(currentPos[0] - 1, currentPos[1]) ) {
-            maze[currentPos[0]][currentPos[1]] = ' ';
-            maze[currentPos[0]-1][currentPos[1]] = 'O';
-            currentPos[0] = currentPos[0] - 1;
-            return true;
-        }
-        return false;
-    }
-
-    bool moveDown() {
-        if (!checkObstacle(currentPos[0] + 1, currentPos[1]) &&
-            IsInBounds(currentPos[0] + 1, currentPos[1])) {
-            maze[currentPos[0]][currentPos[1]] = ' ';
-            maze[currentPos[0] + 1][currentPos[1]] = 'O';
-            currentPos[0] = currentPos[0] + 1;
-            return true;
-        }
-        return false;
-    }
-
-    bool moveRight() {
-        if (!checkObstacle(currentPos[0], currentPos[1] + 1) &&
-            IsInBounds(currentPos[0], currentPos[1] + 1) ) {
-            maze[currentPos[0]][currentPos[1]] = ' ';
-            maze[currentPos[0]][currentPos[1]+1] = 'O';
-            currentPos[1] = currentPos[1] + 1;
-            return true;
-        }
-        return false;
-        
-    }
-
-    bool moveLeft() {
-        if (!checkObstacle(currentPos[0], currentPos[1] - 1) &&
-            IsInBounds(currentPos[0], currentPos[1] - 1)) {
-            maze[currentPos[0]][currentPos[1]] = ' ';
-            maze[currentPos[0]][currentPos[1] - 1] = 'O';
-            currentPos[1] = currentPos[1] - 1;
-            return true;
-        }
-        return false;
-        
-    }
-
-    bool checkWin(int x, int y) {
-        if (x == WIDTH - 2 && y == HEIGHT - 1) {
-            return true;
-        }
-        return false;
-    }
-
-
-    bool checkObstacle(int x, int y) {
-        if (maze[x][y] == WALL) {
-            return true;
-        }
-        return false;
-    }
-
-
-
-
-
-
-    bool IsInBounds(int x, int y) {
-        if (x < 0 || x >= WIDTH) return false;
-        if (y < 0 || y >= HEIGHT) return false;
+    if (!isObstacle(playerPositions[player][0], playerPositions[player][1] - 1) &&
+        IsInBounds(playerPositions[player][1], playerPositions[player][1] - 1)) {
+        playerPositions[player][1] = playerPositions[player][1] - 1;
         return true;
-
     }
+    return false;
+}
 
-
-};
-
-
-void generateConfig() {
-    if (settings_file.is_open()) {
-        settings_file << "height=13\n";
-        settings_file << "width=43\n";
-        settings_file << "insta_print=true";
-        cout << "Generated settings file. \n";
+bool moveDown(int player) {
+    if (!isObstacle(playerPositions[player][0], playerPositions[player][1] + 1) &&
+        IsInBounds(playerPositions[player][0], playerPositions[player][1] + 1)) {
+        playerPositions[player][1] = playerPositions[player][1] + 1;
+        return true;
     }
-    else {
-        cout << "Could not write to file \n";
+    return false;
+}
+
+bool moveRight(int player) {
+    if (!isObstacle(playerPositions[player][0] + 1, playerPositions[player][1]) &&
+        IsInBounds(playerPositions[player][0] + 1, playerPositions[player][1])) {
+        playerPositions[player][0] = playerPositions[player][0] + 1;
+        return true;
     }
+    return false;
+
+}
+
+bool moveLeft(int player) {
+    if (!isObstacle(playerPositions[player][0] - 1, playerPositions[player][1]) &&
+        IsInBounds(playerPositions[player][0] - 1, playerPositions[player][1])) {
+        playerPositions[player][0] = playerPositions[player][0] - 1;
+        return true;
+    }
+    return false;
+
 }
 
 
 
-int main()
-{
-    temp.close();
 
-    // load settings
-    string temp;
-    int i = 0;
-    while (getline(settings_file, temp)) {
-        settings[i] = temp.substr(temp.find("="), temp.length());
-        cout << settings[i];
-        i++;
-        // width, height, insta_print, 
 
+
+void generateMap(int x, int y){
+    // Starting at the given index, recursively visits every direction in a
+    // randomized order.
+    // Set my current location to be an empty passage.
+    map[x][y] = ' ';
+    // Create an local array containing the 4 directions and shuffle their order.
+    int dirs[4];
+    dirs[0] = NORTH;
+    dirs[1] = EAST;
+    dirs[2] = SOUTH;
+    dirs[3] = WEST;
+
+    for (int i = 0; i < 4; ++i){
+        int r = rand() & 3;
+        int temp = dirs[r];
+        dirs[r] = dirs[i];
+        dirs[i] = temp;
     }
-    if (i < 3) {
-        generateConfig();
-    }
-    settings_file.close();
 
-
-
-    
-    srand(time(NULL));
-    bool running = true;
-    Game game;
-
-    coord.X = 0;
-    coord.Y = 0;
-
-    cout << "Press SPACE to start new game..." << endl;
-    cout << "Press ESC to cancel" << endl;
-    cout << "Controlls: ARROW KEYS, ESC to end." << endl;
-
-    while (running) {
-        
-        
-
-
-        switch (_getch()) {
-        case SPACE:
-            game.generateMaze();
-            game.print();
-            game.run();
-
-            cout << "Press SPACE to start new game..." << endl;
-            cout << "Press ESC to cancel" << endl;
-            cout << "Controlls: ARROW KEYS, ESC to end." << endl;
-
-
-            break;
-        case END:
-            running = false;
-            break;
-        case KEY_UP:
-            //clear();
-            game.generateMaze();
-            game.print();
-            break;
-        default:
-            clear();
-            break;
+    // Loop through every direction and attempt to Visit that direction.
+    for (int i = 0; i < 4; ++i){
+        // dx,dy are offsets from current location. Set them based
+        // on the next direction I wish to try.
+        int dx = 0, dy = 0;
+        switch (dirs[i])
+        {
+        case NORTH: dy = -1; break;
+        case SOUTH: dy = 1; break;
+        case EAST: dx = 1; break;
+        case WEST: dx = -1; break;
         }
-        
+        // Find the (x,y) coordinates of the grid cell 2 spots
+        // away in the given direction.
+        int x2 = x + (dx << 1);
+        int y2 = y + (dy << 1);
+        if (IsInBounds(x2, y2)){
+            //cout <<(int) maze[x2][y2];
+            if (map[x2][y2] == WALL){
+                // (x2,y2) has not been visited yet... knock down the
+                // wall between my current position and that position
+                map[x2 - dx][y2 - dy] = ' ';
+                // Recursively Visit (x2,y2)
+                if (!instaPrint) {
+                    render();
+                }
+                generateMap(x2, y2);
+
+
+            }
+        }
+
+
     }
+}
 
-    dada.close();
 
-
+void playerInput() {
+    for (;;) {
+        if (_kbhit) {
+            switch (_getch()) {
+            case KEY_UP:
+                moveUp(0);
+                break;
+            case KEY_DOWN:
+                moveDown(0);
+                break;
+            case KEY_LEFT:
+                moveLeft(0);
+                break;
+            case KEY_RIGHT:
+                moveRight(0);
+                break;
+            case W:
+                moveUp(1);
+                break;
+            case S:
+                moveDown(1);
+                break;
+            case A:
+                moveLeft(1);
+                break;
+            case D:
+                moveRight(1);
+                break;
+            default:
+                break;
+            }
+        }
+    }
 }
 
 
 
 
 
+int main(){
+    mainMenu();
 
 
-
+}
